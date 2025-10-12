828107c067d7fa921854ef9a2626dbe7b0c2760e
iannil <zhurongx@gmail.com>
2025-10-12 12:36:39 +0800

    20251012123639 on macos

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,19 @@
-#!/bin/zsh
+#!/bin/bash
+# Auto-detect operating system
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    OS_NAME="macos"
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    if grep -qi microsoft /proc/version 2>/dev/null; then
+        OS_NAME="wsl"
+    else
+        OS_NAME="linux"
+    fi
+else
+    OS_NAME="unknown"
+fi
+
 git pull && \
 hugo && \
 git add ./ && \
-git commit -m "$(date +'%Y%m%d%H%M%S') on macos" && \
+git commit -m "$(date +'%Y%m%d%H%M%S') on $OS_NAME" && \
 git push
